c0730028be069f33bd78ca3296ddae02460d4dc5
Himtech <tech.himtech@gmail.com>
2026-04-24 15:57:09 +0530

    gfgfgf

--- src/pages/services/api.ts
+++ src/pages/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1";
+// const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1";
 
-//  const API_BASE_URL = import.meta.env.VITE_API_URL || "https://sms-backend-d19v.onrender.com/api/v1";
+  const API_BASE_URL = import.meta.env.VITE_API_URL || "https://sms-backend-d19v.onrender.com/api/v1";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
